171b3d6a08231c94f249852dcd5f3c81ec8a6c7d
unknown <ana.torresgzz@gmail.com>
2023-04-29 22:16:13 -0600

    add adapter explanation

--- adapter/adapter.js
+++ adapter/adapter.js
@@ -1,3 +1,5 @@
+
+// Class which has two methods, scan and connect
 class RedisDataManager {
     connect() {
         return 'Connecting to DB';
@@ -8,27 +10,32 @@ class RedisDataManager {
     }
 }
 
+// Class which is the legacy class, it has only one method which is getData.
 class DataManager {
     getData() {
         return 'Legacy Data'
     }
 }
 
+/* This is the Adapter class. Instantiates the RedisDataManager class and wraps its connect method
+into a getConnect method. The same technique is used with the scan method of the RedisDataManager class*/
 class Adapter {
     redis = new RedisDataManager();
     getConnect() {
         return this.redis.connect();
     }
     getData() {
-    return this.redis.scan();
+        return this.redis.scan();
     }
 }
 
+// The main place in which we call the methods.
 class Client {
     dataManager
     constructor(dataManager) {
         this.dataManager = dataManager;
     }
+    // These are the methods that will allow us to connect to the RedisDataManager using a method like the legacy Database
     getConnect() {
         console.log(this.dataManager.getConnect());
     }
---
+++ adapter/adapter.md
@@ -0,0 +1,4 @@
+# Adapter
+## Type: Structural
+* Use: Converts the interface of one object so that another object can understand it.
+* It's like a wrapper for a class or an object with a different interface so your new object can use it and performs the required operations you need.
